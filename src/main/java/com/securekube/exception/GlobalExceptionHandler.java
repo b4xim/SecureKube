f54cefa61de2dc796f946a9588dce989c7350840
B4xim <bk6549@srmist.edu.in>
2026-08-27 19:28:28 +0530

    Testing webhook

--- src/main/java/com/securekube/exception/GlobalExceptionHandler.java
+++ src/main/java/com/securekube/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+
 import java.time.Instant;
 import java.util.Map;
 
